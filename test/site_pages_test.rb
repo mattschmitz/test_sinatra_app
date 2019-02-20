b7697dd0296de13e09ab0994e827ce921f7f840a
Matt Schmitz <mattschmitz9@gmail.com>
2019-02-19 16:28:55 -0800

    add nav bar

--- test/site_pages_test.rb
+++ test/site_pages_test.rb
@@ -3,6 +3,7 @@ require_relative 'test_helper'
 class PalindromeAppTest < Minitest::Test
  include Rack::Test::Methods
 
+  TITLE = "Toy Sinatra App"
   def app
     Sinatra::Application
   end
@@ -10,19 +11,29 @@ class PalindromeAppTest < Minitest::Test
   def test_index
     get '/'
     assert last_response.ok?
-    assert doc(last_response).at_css('h1')
+    document = doc(last_response)
+    assert document.at_css('h1')
+    assert document.at_css('nav')
+    title_content = document.at_css('title').content
+    assert_equal "#{TITLE} | Home", title_content  
   end
 
   def test_about
     get '/about'
     assert last_response.ok?
-    assert doc(last_response).at_css('h1')
+    document = doc(last_response)
+    assert document.at_css('h1')
+    title_content = document.at_css('title').content
+    assert_equal "#{TITLE} | About", title_content
   end
 
   def test_palindrome
     get '/palindrome'
     assert last_response.ok?
-    assert doc(last_response).at_css('h1')
+    document = doc(last_response)
+    assert document.at_css('h1')
+    title_content = document.at_css('title').content
+    assert_equal "#{TITLE} | Palindrome", title_content
   end
 
 end
